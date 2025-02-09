69630ff77cdc0b00ee203e84e0172999a4dec516
Szymon <szymon.wais@gmail.com>
2025-02-09 20:57:11 +0100

    Dodana strona o serwisie i view i url

--- app/geoportal_frontend/urls.py
+++ app/geoportal_frontend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import map_view
+from .views import map_view, about_view
 
 
 urlpatterns = [
     path('', map_view, name='map'),
+    path('about/', about_view, name='about'),
 ]
--- app/geoportal_frontend/views.py
+++ app/geoportal_frontend/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def map_view(request):
     return render(request, 'map.html')
+
+
+def about_view(request):
+    return render(request, 'about.html')
